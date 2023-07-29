94b1b4083071f8cc56b6f55e681ee51d596b3f89
alexhetley6107 <hetleyofficial@gmail.com>
2023-07-29 17:28:38 +0300

    fix(eslint)

--- src/shared/lib/colorModeContext/index.ts
+++ src/shared/lib/colorModeContext/index.ts
@@ -5,7 +5,6 @@ interface IThemeState {
 }
 
 export const ColorModeContext = createContext<IThemeState>({
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   toggleColorMode: () => {},
 });
 
