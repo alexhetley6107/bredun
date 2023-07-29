9722c44ce57f6e5edef74bc70ca5bd8116701323
alexhetley6107 <hetleyofficial@gmail.com>
2023-07-29 17:50:41 +0300

    fix(scrolbar)

--- src/shared/ui/Layout/index.tsx
+++ src/shared/ui/Layout/index.tsx
@@ -1,7 +1,9 @@
-import { Box, Container } from '@mui/material';
+import { Box, Container, useMediaQuery } from '@mui/material';
 import React, { FC, PropsWithChildren } from 'react';
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
+  const isTablet = useMediaQuery('(max-width:650px)');
+
   return (
     <Box
       sx={{
@@ -11,12 +13,12 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
         bgcolor: 'secondary.light',
         '&::-webkit-scrollbar-thumb': {
           backgroundColor: 'secondary.dark',
-          border: `5px solid white`,
+          border: isTablet ? '3px solid white' : `5px solid white`,
           borderColor: `secondary.light`,
           borderRadius: '30px',
         },
         '&::-webkit-scrollbar': {
-          width: '16px',
+          width: isTablet ? '8px' : '16px',
         },
       }}
     >
